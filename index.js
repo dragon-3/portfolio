ee1512b72f4da65fda757810bbd1d53dc3a12228
dragon_3 <seannarron9@gmail.com>
2021-03-11 21:29:22 +0900

    Fourth commit

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-//nav bar
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
-
-var myIndex = 0;
-carousel();
-
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("profile prof-text");
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 2500);    
-}
-
-
-
 //Movement Animation to happen
 const frameOne = document.querySelector(".frame-one");
 const frameTwo = document.querySelector(".frame-two");
